5902af361fbc16205ee6afe9c514fedd1b36daf2
maitykarabi <60187353+maitykarabi@users.noreply.github.com>
2025-08-11 15:31:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeElements = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    });
+
+    fadeElements.forEach(el => observer.observe(el));
+});
