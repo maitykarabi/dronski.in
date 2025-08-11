47491bdd8c95b67fcfcc798ae90e717c52a11b37
maitykarabi <60187353+maitykarabi@users.noreply.github.com>
2025-08-11 16:11:57 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "dronski-website",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "export": "next export"
+  },
+  "dependencies": {
+    "framer-motion": "latest",
+    "next": "latest",
+    "react": "latest",
+    "react-dom": "latest"
+  },
+  "devDependencies": {
+    "autoprefixer": "latest",
+    "postcss": "latest",
+    "tailwindcss": "latest"
+  }
+}
