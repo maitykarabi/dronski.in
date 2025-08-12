c981dca049795f83d736a8a0628e636bcbb70217
maitykarabi <60187353+maitykarabi@users.noreply.github.com>
2025-08-12 10:00:10 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',            // enable static export
-  images: { unoptimized: true } // disable Next image optimizer for static hosting
-  // basePath: '' // keep empty for root domain like dronski.in
-};
-
-module.exports = nextConfig;
